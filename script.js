1036ea429495c010ec06369411199bae6607990b
GrishaRubitel <102182149+GrishaRubitel@users.noreply.github.com>
2023-12-04 23:42:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function updateHistory(inText, perc) {
     document.getElementById("inputRequest").value = "";
 }
 
-var host = "http://bac0-188-242-233-156.ngrok-free.app:8080";
+var host = "http://semantle.ru:8080";
 var sypheredWord;
 var maxPerc;
 
